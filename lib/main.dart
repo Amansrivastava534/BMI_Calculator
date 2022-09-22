2aa2829d1c15af976c516a54192c69aca85ed796
Amansrivastava534 <Amansrivastava534@gmail.com>
2022-09-22 21:28:26 +0530

    Day_one

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bmi_calculator/input_page.dart';
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21)
+      ),
+      home: InputPage(),
+    );
+
+  }
+}
+
+
